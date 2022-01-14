3a480101495c1f7beb812a93860c84d0f47d62c1
Jehan Tremback <hi@jehan.email>
2022-01-14 11:30:58 -0800

    chain is starting

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,10 @@ ENV PATH=$PATH:/go/bin
 RUN dnf install -y git make gcc gcc-c++ which iproute iputils procps-ng vim-minimal tmux net-tools htop tar jq npm openssl-devel perl rust cargo golang
 
 # Copy in the repo under test
-ADD ./cosmos-gravity-bridge /cosmos-gravity-bridge
+ADD ./interchain-security /interchain-security
 
 # Build the Go module
-RUN pushd /cosmos-gravity-bridge/module/ && PATH=$PATH:/usr/local/go/bin GOPROXY=https://proxy.golang.org make && PATH=$PATH:/usr/local/go/bin make install
+RUN pushd /interchain-security/ && PATH=$PATH:/usr/local/go/bin GOPROXY=https://proxy.golang.org make && PATH=$PATH:/usr/local/go/bin make install
 
 # Copy in the shell scripts that run the testnet
 ADD ./testnet-scripts /testnet-scripts
--- testnet-scripts/setup-validators.sh
+++ testnet-scripts/setup-validators.sh
@@ -2,9 +2,9 @@
 set -eux
 
 # your gaiad binary name
-BIN=gravity
+BIN=interchain-securityd
 
-CHAIN_ID="gravity-test"
+CHAIN_ID="interchain-security-provider"
 
 NODES=$1
 
@@ -22,11 +22,8 @@ $BIN init $STARTING_VALIDATOR_HOME --chain-id=$CHAIN_ID validator1
 ## we could keep a hardcoded genesis file around but that would prevent us from
 ## testing the generated one with the default values provided by the module.
 
-# add in denom metadata for both native tokens
-jq '.app_state.bank.denom_metadata += [{"base": "footoken", display: "mfootoken", "description": "A non-staking test token", "denom_units": [{"denom": "footoken", "exponent": 0}, {"denom": "mfootoken", "exponent": 6}]}, {"base": "stake", display: "mstake", "description": "A staking test token", "denom_units": [{"denom": "stake", "exponent": 0}, {"denom": "mstake", "exponent": 6}]}]' /validator$STARTING_VALIDATOR/config/genesis.json > /metadata-genesis.json
-
 # a 60 second voting period to allow us to pass governance proposals in the tests
-jq '.app_state.gov.voting_params.voting_period = "60s"' /metadata-genesis.json > /edited-genesis.json
+jq '.app_state.gov.voting_params.voting_period = "60s"' /validator$STARTING_VALIDATOR/config/genesis.json > /edited-genesis.json
 
 mv /edited-genesis.json /genesis.json
 
@@ -42,7 +39,7 @@ do
     # Generate a validator key, orchestrator key, and eth key for each validator
     $BIN keys add $ARGS validator$i 2>> /validator-phrases
     $BIN keys add $ARGS orchestrator$i 2>> /orchestrator-phrases
-    $BIN eth_keys add >> /validator-eth-keys
+    # $BIN eth_keys add >> /validator-eth-keys
 
     VALIDATOR_KEY=$($BIN keys show validator$i -a $ARGS)
     ORCHESTRATOR_KEY=$($BIN keys show orchestrator$i -a $ARGS)
@@ -59,14 +56,11 @@ done
 for i in $(seq 1 $NODES);
 do
     cp /genesis.json /validator$i/config/genesis.json
-    GAIA_HOME="--home /validator$i"
-    ARGS="$GAIA_HOME --keyring-backend test"
-    ORCHESTRATOR_KEY=$($BIN keys show orchestrator$i -a $ARGS)
-    ETHEREUM_KEY=$(grep address /validator-eth-keys | sed -n "$i"p | sed 's/.*://')
+
     # the /8 containing 7.7.7.7 is assigned to the DOD and never routable on the public internet
     # we're using it in private to prevent gaia from blacklisting it as unroutable
     # and allow local pex
-    $BIN gentx $ARGS $GAIA_HOME --moniker validator$i --chain-id=$CHAIN_ID --ip 7.7.7.$i validator$i 500000000stake $ETHEREUM_KEY $ORCHESTRATOR_KEY
+    $BIN gentx --home /validator$i --keyring-backend test --moniker validator$i --chain-id=$CHAIN_ID --ip 7.7.7.$i validator$i 500000000stake
     # obviously we don't need to copy validator1's gentx to itself
     if [ $i -gt 1 ]; then
         cp /validator$i/config/gentx/* /validator1/config/gentx/
