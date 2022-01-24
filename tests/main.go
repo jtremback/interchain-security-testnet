00e6c1f81adc2f0edafd6f54d655ddc2013d8879
Jehan Tremback <hi@jehan.email>
2022-01-24 14:47:31 -0800

    successfully waiting for chain start and sending a coin

--- tests/main.go
+++ tests/main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"regexp"
+	"time"
 )
 
 // property erupt day common remind oblige chunk thumb jazz camera erupt reward divorce fit toy cargo traffic scrub begin gown recall video friend prosper
@@ -29,22 +31,83 @@ func main() {
 
 	println("chain started?")
 
-	// docker exec interchain-security-instance interchain-securityd tx gov submit-proposal --title="Test Proposal" --description="My awesome proposal" --type Text --deposit 10000000stake --from validator1 --chain-id provider --home /provider/validator1 --keyring-backend test
-	bz, _ := exec.Command("docker", "exec", "interchain-security-instance", "interchain-securityd", "tx", "gov", "submit-proposal",
-		`--title="Test Proposal"`,
-		`--description="My awesome proposal"`,
-		`--type`, `Text`,
-		`--deposit`, `10000000stake`,
-		`--from`, `validator1`,
+	// ms := 0
+	// for {
+	// 	println(fmt.Sprint(ms) + " MILLISECONDS ----------------------------------------------")
+
+	// 	bz, _ := exec.Command("docker", "exec", "interchain-security-instance", "interchain-securityd", "query", "bank", "balances",
+	// 		`cosmos19pe9pg5dv9k5fzgzmsrgnw9rl9asf7ddwhu7lm`,
+	// 		`--chain-id`, `provider`,
+	// 		`--home`, `/provider/validator1`,
+	// 	).CombinedOutput()
+	// 	fmt.Println(string(bz))
+
+	// 	// docker exec interchain-security-instance interchain-securityd query block
+	// 	bz, _ = exec.Command("docker", "exec", "interchain-security-instance", "interchain-securityd", "query", "block",
+	// 		`--chain-id`, `provider`,
+	// 		`--home`, `/provider/validator1`,
+	// 	).CombinedOutput()
+	// 	// fmt.Println(string(bz) == `{"block_id":{"hash":"","parts":{"total":0,"hash":""}},"block":null}`)
+	// 	fmt.Println(regexp.Match(`{"block_id":{"hash":"","parts":{"total":0,"hash":""}},"block":null}`, bz))
+
+	// 	time.Sleep(100 * time.Millisecond)
+	// 	ms += 100
+	// }
+
+	// // docker exec interchain-security-instance interchain-securityd tx gov submit-proposal --title="Test Proposal" --description="My awesome proposal" --type Text --deposit 10000000stake --from validator1 --chain-id provider --home /provider/validator1 --keyring-backend test
+	// bz, _ := exec.Command("docker", "exec", "interchain-security-instance", "interchain-securityd", "tx", "gov", "submit-proposal",
+	// 	`--title="Test Proposal"`,
+	// 	`--description="My awesome proposal"`,
+	// 	`--type`, `Text`,
+	// 	`--deposit`, `10000000stake`,
+	// 	`--from`, `validator1`,
+	// 	`--chain-id`, `provider`,
+	// 	`--home`, `/provider/validator1`,
+	// 	`--keyring-backend`, `test`,
+	// ).CombinedOutput()
+
+	// fmt.Println(string(bz))
+
+	bz, _ := exec.Command("docker", "exec", "interchain-security-instance", "interchain-securityd", "query", "bank", "balances",
+		`cosmos19pe9pg5dv9k5fzgzmsrgnw9rl9asf7ddwhu7lm`,
+		`--chain-id`, `provider`,
+		`--home`, `/provider/validator1`,
+	).CombinedOutput()
+	fmt.Println(string(bz))
+
+	// docker exec interchain-security-instance interchain-securityd tx bank send cosmos19pe9pg5dv9k5fzgzmsrgnw9rl9asf7ddwhu7lm cosmos1dkas8mu4kyhl5jrh4nzvm65qz588hy9qcz08la 1stake --home /provider/validator1 --keyring-backend test --chain-id provider -y
+	bz, _ = exec.Command("docker", "exec", "interchain-security-instance", "interchain-securityd", "tx", "bank", "send",
+		`cosmos19pe9pg5dv9k5fzgzmsrgnw9rl9asf7ddwhu7lm`,
+		`cosmos1dkas8mu4kyhl5jrh4nzvm65qz588hy9qcz08la`,
+		`1stake`,
 		`--chain-id`, `provider`,
 		`--home`, `/provider/validator1`,
 		`--keyring-backend`, `test`,
+		`-b`, `block`,
+		`-y`,
+	).CombinedOutput()
+
+	fmt.Println(string(bz))
+
+	// docker exec interchain-security-instance interchain-securityd query bank balances cosmos19pe9pg5dv9k5fzgzmsrgnw9rl9asf7ddwhu7lm --home /provider/validator1 --chain-id provider
+	bz, _ = exec.Command("docker", "exec", "interchain-security-instance", "interchain-securityd", "query", "bank", "balances",
+		`cosmos19pe9pg5dv9k5fzgzmsrgnw9rl9asf7ddwhu7lm`,
+		`--chain-id`, `provider`,
+		`--home`, `/provider/validator1`,
 	).CombinedOutput()
 
 	fmt.Println(string(bz))
 
 	outChannel := make(chan string)
 	<-outChannel
+}
+
+// Waits a number of blocks
+func waitBlocks(num uint) {
+	for {
+
+		time.Sleep(100 * time.Millisecond)
+	}
 }
 
 func start_docker(container_name string, instance_name string, expose_ports ...uint) {
@@ -115,12 +178,34 @@ func start_chain(
 	for scanner.Scan() {
 		out := scanner.Text()
 		fmt.Println("start chain: " + out)
-		if out == "done!!!!!!!!" {
-			return
-		}
+		// if out == "done!!!!!!!!" {
+		// 	return
+		// }
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
+	}
+
+	println("polling for chain start")
+	for {
+		// docker exec interchain-security-instance interchain-securityd query block
+		bz, _ := exec.Command("docker", "exec", "interchain-security-instance", "interchain-securityd", "query", "block",
+			`--chain-id`, `provider`,
+			`--home`, `/provider/validator1`,
+		).CombinedOutput()
+
+		fmt.Println(string(bz))
+
+		match, err := regexp.Match(`{"block_id":{"hash":"","parts":{"total":0,"hash":""}},"block":null}`, bz)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if !match {
+			return
+		}
+
+		time.Sleep(100 * time.Millisecond)
 	}
 }
 
